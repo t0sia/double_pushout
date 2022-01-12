e474c00a6021f9e9cc8835d15206859d9f4dfc9e
domkvv <niczkaa.bo@gmail.com>
2022-01-12 23:22:22 +0100

    better parsing

--- src/parse.py
+++ src/parse.py
@@ -1,41 +1,30 @@
 from matplotlib import pyplot as plt
 from structures import Grammar, Graph, Production
 
-
 def get_breaks(f):
     breaks = []
-    dicts = []
-    cut = 0
-    dict = 0
     for i, line in f:
 
         if line == "-->\n":
             labels = i + 1
-            start = i + 2
+            indexes = i + 2
+            start = i + 3
 
         elif line == "--<\n":
             stop = i - 1
+            breaks.append((labels, indexes, start, stop))
 
-            if breaks != []:
-                cut += 1
-
-            if cut % 3 == 0:
-                dict = 0
-            breaks.append((labels, start, stop))
-
-        elif line == "---\n" and cut % 3 == 0 and dict == 0:
-            dict1 = i + 1
-            dict2 = i + 3
-            dict = 1
-            dicts.append((dict1, dict2))
-
-    return breaks, dicts
+    return breaks
 
 
-def read_vlabels(f, s):
-    line = f[s][1]
-    vlabels = line.split()
-    return vlabels
+def read_vlabels(f, labels, indexes):
+    i = f[indexes][1].split()
+    l = f[labels][1].split()
+    vlabels = []
+    for j in range(len(l)):
+        vlabels.append((int(i[j]), l[j]))
+
+    return dict(vlabels)
 
 
 def read_edges(f, start, stop):
@@ -53,54 +42,42 @@ def get_edge(s):
     return res
 
 
-def edges_to_adj(e, n):
-    adj = [[] for _ in range(n)]
+def edges_to_adj(e, n, labels):
+    adj = [[] for _ in range(n+1)]
     for u, s, v in e:
         adj[u].append((v, s))
         adj[v].append((u, s))
-    return adj
+
+    out = []
+    for u in labels:
+        out.append((u, adj[u]))
+
+    return dict(out)
 
 
-def read_dict(f, d):
-    line = f[d][1]
-    dict = line.split()
-    for el in dict:
-        if el == "-":
-            el = None
-        else:
-            el = int(el)
-
-    return dict
-
-
-def make_dict(f, d1, d2):
-    S = []
-    S.append((read_dict(f, d1)))
-    S.append((read_dict(f, d2)))
-    return S
-
-
-def get_graph(f, labels, start, stop):
-    vlabels = read_vlabels(f, labels)
+def get_graph(f, labels, indexes, start, stop):
+    vlabels = read_vlabels(f, labels, indexes)
+    n = 0
+    for label in vlabels:
+        n = max(n, label)
     edges = read_edges(f, start, stop)
-    adjacency_list = edges_to_adj(edges, len(vlabels))
+    adjacency_list = edges_to_adj(edges, n, vlabels)
     return Graph(vlabels, adjacency_list)
 
 
 def parse(f):
     ef = list(enumerate(f))
-    breaks, dicts = get_breaks(ef)
-    G = get_graph(ef, breaks[0][0], breaks[0][1], breaks[0][2])
+    breaks = get_breaks(ef)
+    G = get_graph(ef, breaks[0][0], breaks[0][1], breaks[0][2], breaks[0][3] )
 
     prods = []
     lkr = []
     for i in range(1, len(breaks)):
-        labels, start, stop = breaks[i]
-        g = get_graph(ef, labels, start, stop)
+        labels, indexes, start, stop = breaks[i]
+        g = get_graph(ef, labels, indexes, start, stop)
         lkr.append(g)
         if i % 3 == 0:
-            dict = make_dict(ef, dicts[i // 3 - 1][0], dicts[i // 3 - 1][1])
-            prods.append(Production(lkr[0], lkr[1], lkr[2], dict))
+            prods.append(Production(lkr[0], lkr[1], lkr[2]))
             lkr = []
 
     return Grammar(G, prods)
@@ -109,10 +86,11 @@ def parse(f):
 # test co
 if __name__ == "__main__":
 
-    with open("ex3.txt") as f:
+    with open("src\ex1.txt") as f:
         grammar = parse(f)
         g = grammar.input_graph
         prods = grammar.production_list
+
         g.visualize()
         plt.show()
 
